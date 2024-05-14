241b4dd77b34230479052e649c9742db389c632a
Thomas Flensted <thomasflenstedjensen@gmail.com>
2024-05-14 10:46:15 +0200

    cleared readme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 import NavBar from "./ui/global/NavBar";
 
 const inter = Inter({ subsets: ["latin"] });
